3f9d4e53a08b3ce0e31680b4dd39ea03f1c3ad3a
Sachin Sourabh <Sachin.Sourabh@MSBC-MAINFRAME.LCL>
2022-09-07 06:53:23 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// function reset() {
+//     document.getElementById("registrationform").reset();
+//  }
+
+//  function submit() {
+//     location.replace("https://www.w3schools.com")
+//   }
+
+  function reset(){
+    document.getElementById('FirstName').value='';
+    document.getElementById('Dob').value='';
+    document.getElementById('male').value='';
+    document.getElementById('female').value='';
+    document.getElementById('lastname').value='';
+    document.getElementById('email').value='';
+    document.getElementById('contact').value='';
+    }
